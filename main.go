688bc21282d99c8aa876395731c43e8b862fca78
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-04-10 16:17:29 +0200

    chore: Reduce payload size (#16)

--- main.go
+++ main.go
@@ -11,26 +11,19 @@ import (
 )
 
 func main() {
-	// Charchement .env
 	_ = godotenv.Load()
 
-	// Tentative de connexion à la base de donnée
 	data.ConnectToDB()
-	// Programmation de la fermeture de la base de données à la fermeture du programme
 	defer data.CloseDB()
 
-	// Création du routeur avec le framework GIN
 	router := gin.Default()
 
-	// Création de la configuration par défaut des cors du serveur
 	configCors := cors.DefaultConfig()
 
-	// Modification des paramètres
 	configCors.AllowAllOrigins, configCors.AllowCredentials = true, true
 	configCors.AddAllowHeaders("Authorization")
 	configCors.AddAllowHeaders("creditential")
 
-	// Application de la nouvelle configuration
 	router.Use(cors.New(configCors))
 
 	// users
@@ -46,6 +39,5 @@ func main() {
 
 	// commentaires
 
-	// Lancement du serveur
 	_ = router.Run("localhost:8088")
 }
